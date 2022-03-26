39dcaa01221b479774a44ed47d7b8335c98464f7
Yesica N Mendoza <yesicanmendoza@gmail.com>
2022-03-26 16:58:22 +0000

    functions complete

--- functions.js
+++ functions.js
@@ -1,13 +1,49 @@
 'use strict';
 
 // 1. isHometown
+function isHometown(town){
+    //Given a `town`, return `True` if `town` is 'San Francisco'.
+    return town === "San Francisco";  
+ }
 
-// Define your function here
+
 
 // 2. getFullName
+function getFullName(first_name, last_name){
+  //Given `first_name` and `last_name`, return a full name.
 
-// Define your function here
+  return `${first_name} ${last_name}`;
+}
+
+
 
 // 3. calculateTotal
 
-// Define your function here
+function calculateTotal(base_price, state, tax=0.05){
+    //Return the total price of an item, figuring in state taxes and fees.
+
+   
+    const subtotal = base_price * (1 + tax);
+
+    let fee = 0;
+    if (state === "CA"){
+        fee = 0.03 * subtotal;
+    }
+    else{
+        if (state === "PA"){
+        fee = 2;
+        }
+    
+        if (state === "MA"){
+            if (base_price <= 100){
+                fee = 1;
+            }
+            else{
+            fee = 3;
+            }
+        }
+    } 
+
+    return subtotal + fee;
+
+}
